edf643fb894ede732f560b89fd6b7cefe29fd79e
LuisLDA <65144234+LuisLDA@users.noreply.github.com>
2021-03-02 17:00:57 -0500

    Initial commit

---
+++ src/main/java/com/inventario/sistemainv/SistemainvApplication.java
@@ -0,0 +1,13 @@
+package com.inventario.sistemainv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemainvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemainvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inventario/sistemainv/SistemainvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventario.sistemainv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemainvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
