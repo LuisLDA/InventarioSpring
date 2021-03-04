74893652a934c1f9fcf502e99ce5c07ceb65db7e
LuisLDA <65144234+LuisLDA@users.noreply.github.com>
2021-03-04 07:15:02 -0500

    agregado mas controladores con paginas html

--- src/main/java/com/inventario/sistemainv/controlador/ControladorInicio.java
+++ src/main/java/com/inventario/sistemainv/controlador/ControladorInicio.java
@@ -3,6 +3,7 @@ package com.inventario.sistemainv.controlador;
 import com.inventario.sistemainv.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,7 +21,27 @@ public class ControladorInicio {
     public String inicio(Model model) {
         log.info("INICIANDO EL CONTROLADOR...");
         var usuarios = userService.listUser();
-        model.addAttribute("users",usuarios);
+        model.addAttribute("users", usuarios);
         return "home";
+    }
+
+    @GetMapping("/media")
+    public String media(Model model) {
+        return "media";
+    }
+
+    @GetMapping("/accesos")
+    public String controlAcceso(User user) {
+        return "accesos";
+    }
+
+    @GetMapping("/productos")
+    public String mostrarProductos(Model model) {
+        return "productos";
+    }
+
+    @GetMapping("/categorias")
+    public String mostrarCategorias(Model model) {
+        return "categorias";
     }
 }
