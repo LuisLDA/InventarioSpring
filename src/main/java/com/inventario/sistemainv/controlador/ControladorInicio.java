f769696b481d4150e63b28058c95fb6c8fd3bdcb
DarkMSoul <68128219+DarkMSoul@users.noreply.github.com>
2021-03-04 00:35:00 -0500

    Tabla categories

--- src/main/java/com/inventario/sistemainv/controlador/ControladorInicio.java
+++ src/main/java/com/inventario/sistemainv/controlador/ControladorInicio.java
@@ -1,5 +1,6 @@
 package com.inventario.sistemainv.controlador;
 
+import com.inventario.sistemainv.service.CategoriesService;
 import com.inventario.sistemainv.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
@@ -16,11 +17,28 @@ public class ControladorInicio {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private CategoriesService categoriesService;
+
     @GetMapping("/")
     public String inicio(Model model) {
         log.info("INICIANDO EL CONTROLADOR...");
         var usuarios = userService.listUser();
         model.addAttribute("users",usuarios);
+        var categoria = categoriesService.listCategories();
+        model.addAttribute("categorie", categoria);
         return "home";
     }
+
+    /*
+    @Autowired
+    private CategoriesService categoriesService;
+
+    @GetMapping("/")
+    public String categorie(Model model) {
+        log.info("INICIANDO CONTROLADOR...");
+        var categoria = categoriesService.listCategories();
+        model.addAttribute("categorie", categoria);
+        return "home";
+    }*/
 }
