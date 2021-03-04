e8ee537b429f65104b75603d69d0694e5bc13d0d
LuisLDA <65144234+LuisLDA@users.noreply.github.com>
2021-03-03 20:10:33 -0500

    Implementacion primaria del CRUD aun intentando hacerlo funcionar >:v

---
+++ src/main/java/com/inventario/sistemainv/controlador/ControladorInicio.java
@@ -0,0 +1,26 @@
+package com.inventario.sistemainv.controlador;
+
+import com.inventario.sistemainv.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import lombok.var;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        log.info("INICIANDO EL CONTROLADOR...");
+        var usuarios = userService.listUser();
+        model.addAttribute("users",usuarios);
+        return "home";
+    }
+}
