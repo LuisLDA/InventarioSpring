4a36a40ab2fa288899c7ee26a88387ab31e8ff73
LuisLDA <65144234+LuisLDA@users.noreply.github.com>
2021-03-25 11:15:51 -0500

    Cambio total de la interfaz con ayuda de bootstrap studio * bootsttrap actualizado a la version 4.6 * main css editado para adaptarse * cambio a la plantilla * cambiado glyphicon por font awoseno de google * agregado librerias de bootstrap 4.6 (Las de webjar no funcionan bien) *agregado js y archivos extras

--- src/main/java/com/inventario/sistemainv/web/SecurityConfig.java
+++ src/main/java/com/inventario/sistemainv/web/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/webjars/**","/css/**","/images/**","/imagenes/").permitAll()
+        http.authorizeRequests().antMatchers("/webjars/**","/css/**","/images/**","/imagenes/","/fonts/**","/bootstrap/**","/js/**").permitAll()
                 .antMatchers("/accesos/**")
                 .hasAuthority("1")
                 .anyRequest().authenticated()
