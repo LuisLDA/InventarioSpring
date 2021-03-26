45a420f02d7f8589487a1a85d95ebfcc25f8ec99
DarkMSoul <68128219+DarkMSoul@users.noreply.github.com>
2021-03-26 15:13:56 -0500

    Mejora vista PDF

--- src/main/java/com/inventario/sistemainv/view/pdf/ventasPdfView.java
+++ src/main/java/com/inventario/sistemainv/view/pdf/ventasPdfView.java
@@ -1,19 +1,19 @@
 package com.inventario.sistemainv.view.pdf;
 
 import com.inventario.sistemainv.domain.Ventas;
-import com.lowagie.text.Document;
-import com.lowagie.text.FontFactory;
-import com.lowagie.text.Phrase;
-import com.lowagie.text.pdf.PdfPCell;
-import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfTable;
-import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.*;
+import com.lowagie.text.Image;
+import com.lowagie.text.pdf.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -29,26 +29,33 @@ public class ventasPdfView extends AbstractPdfView {
         List<Ventas> ventasList = (List<Ventas>) model.get("ventas");
         PdfPCell cell = null;
 
-        PdfPTable encabezado = new PdfPTable(1);
+        PdfPTable encabezado = new PdfPTable(3);
+        encabezado.setWidthPercentage(100);
+        encabezado.setSpacingAfter(20);
+        encabezado.setHorizontalAlignment(PdfPTable.ALIGN_LEFT);
         encabezado.addCell(img(IMAGEN));
-        encabezado.setSpacingAfter(25);
+        encabezado.addCell(encabezadoText("LA ECUATORIANA S.A."));
+        encabezado.addCell(textinfo("Versalles N22-87 y Ramirez Dávalos Esq."));
+        encabezado.addCell(textinfo("2229100-2942900"));
+        encabezado.addCell(textinfo("Quito-Ecuador"));
         document.add(encabezado);
 
         PdfPTable title = new PdfPTable(1);
         title.addCell(titleText("Reporte General de Ventas"));
-        title.setSpacingAfter(25);
+        title.setSpacingAfter(17);
         document.add(title);
 
         PdfPTable report = new PdfPTable(1);
-        report.setWidthPercentage(32);
-        report.setSpacingAfter(25);
+        report.setWidthPercentage(29);
+        report.setSpacingAfter(17);
         report.setHorizontalAlignment(PdfTable.ALIGN_RIGHT);
         report.addCell(reportText("Reporte Generado"));
-        report.addCell("fecha");
+        report.addCell(date());
         document.add(report);
 
         PdfPTable ventasTable = new PdfPTable(6);
         ventasTable.setWidthPercentage(110);
+        ventasTable.setWidths(new float[] {1.5f, 3.5f, 1.5f, 1.5f, 1.5f, 1});
         ventasTable.setSpacingAfter(25);
         ventasTable.addCell(ventasTableHead("Fecha"));
         ventasTable.addCell(ventasTableHead("Producto"));
@@ -60,7 +67,7 @@ public class ventasPdfView extends AbstractPdfView {
         total = BigDecimal.ZERO;
         for (Ventas ventas: ventasList) {
             ventasTable.addCell(ventasTableBody(ventas.getDate()));
-            ventasTable.addCell(ventasTableBody(ventas.getProduct_id().getName()));
+            ventasTable.addCell(tableBodyProduc(ventas.getProduct_id().getName()));
             ventasTable.addCell(ventasTableBody(ventas.getProduct_id().getBuy_price().toString()));
             ventasTable.addCell(ventasTableBody(ventas.getProduct_id().getSale_price().toString()));
             ventasTable.addCell(ventasTableBody(ventas.getQty().toString()));
@@ -70,41 +77,88 @@ public class ventasPdfView extends AbstractPdfView {
 
         cell = new PdfPCell(new Phrase("Total"));
         cell.setColspan(5);
+        cell.setPadding(5);
         cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
         ventasTable.addCell(cell);
-        ventasTable.addCell(total.toString());
+        ventasTable.addCell(ventasTableBody(total.toString()));
 
         document.add(ventasTable);
-
-
 
     }
 
     public PdfPCell img(String path) {
         PdfPCell cell = new PdfPCell();
+        Image image;
+        try {
+            image = Image.getInstance(IMAGEN);
+            cell.addElement(image);
+            cell.setRowspan(4);
+            cell.setBorderWidth(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cell;
+    }
+
+    public static PdfPCell encabezadoText(String text) {
+        PdfPCell cell = new PdfPCell();
+        cell.setColspan(2);
+        cell.setBorderWidth(0);
+        cell.setPhrase(new Phrase(text));
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        cell.setVerticalAlignment(PdfPCell.ALIGN_MIDDLE);
+        return cell;
+    }
+
+    public static PdfPCell textinfo(String text) {
+        PdfPCell cell = new PdfPCell();
+        cell.setColspan(2);
+        cell.setPadding(2);
+        cell.setBorderWidth(0);
+        cell.setPhrase(new Phrase(text, FontFactory.getFont(FontFactory.TIMES, 9)));
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        cell.setVerticalAlignment(PdfPCell.ALIGN_TOP);
         return cell;
     }
 
     public static PdfPCell titleText(String text) {
         PdfPCell cell = new PdfPCell();
-        cell.setPhrase(new Phrase(text));
+        cell.setBorderWidth(0);
+        cell.setPhrase(new Phrase(text, FontFactory.getFont(FontFactory.HELVETICA_BOLD, 17)));
         cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
         cell.setVerticalAlignment(PdfPCell.ALIGN_MIDDLE);
+        cell.setPadding(8);
         return cell;
     }
 
     public static PdfPCell reportText(String text) {
         PdfPCell cell = new PdfPCell();
         cell.setPhrase(new Phrase(text, FontFactory.getFont(FontFactory.HELVETICA_BOLD, 14)));
+        cell.setBackgroundColor(new Color(167, 162, 162));
         cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        cell.setVerticalAlignment(PdfPCell.ALIGN_MIDDLE);
+        cell.setPadding(7);
+        return cell;
+    }
+
+    public static PdfPCell date() {
+        PdfPCell cell = new PdfPCell();
+        SimpleDateFormat fecha = new SimpleDateFormat("dd-MM-yyyy - HH:mm");
+        String date = fecha.format(new Date());
+        cell.setPhrase(new Phrase(date));
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        cell.setVerticalAlignment(PdfPCell.ALIGN_MIDDLE);
+        cell.setPadding(7);
         return cell;
     }
 
     public static PdfPCell ventasTableHead(String text) {
         PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(new Color(56, 135, 199));
         cell.setPhrase(new Phrase(text));
         cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
         cell.setVerticalAlignment(PdfPCell.ALIGN_MIDDLE);
+        cell.setPadding(8);
         return cell;
     }
 
@@ -113,6 +167,15 @@ public class ventasPdfView extends AbstractPdfView {
         cell.setPhrase(new Phrase(text));
         cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
         cell.setVerticalAlignment(PdfPCell.ALIGN_MIDDLE);
+        cell.setPadding(5);
+        return cell;
+    }
+
+    public static PdfPCell tableBodyProduc(String text) {
+        PdfPCell cell = new PdfPCell();
+        cell.setPhrase(new Phrase(text));
+        cell.setVerticalAlignment(PdfPCell.ALIGN_MIDDLE);
+        cell.setPadding(5);
         return cell;
     }
 }
