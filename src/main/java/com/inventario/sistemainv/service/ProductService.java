52a6e58143b14a34403501aeb2a48d0e7e8f2584
LuisLDA <65144234+LuisLDA@users.noreply.github.com>
2021-03-28 18:32:36 -0500

    * Correcciones Menores

--- src/main/java/com/inventario/sistemainv/service/ProductService.java
+++ src/main/java/com/inventario/sistemainv/service/ProductService.java
@@ -5,19 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface ProductService {
-    public List<Product> listProduct();
-    public void saveProduct(Product product);
-    public void deleteProduct(Product product);
-    public Product searchProduct(Product product);
+    List<Product> listProduct();
+    void saveProduct(Product product);
+    void deleteProduct(Product product);
+    Product searchProduct(Product product);
 
-    public int countProducts();
-    public String searchNameProd(String name);
-    public List<Product> productRecient();
+    int countProducts();
+    String searchNameProd(String name);
+    List<Product> productRecient();
 
-    public List<String> names_products();
-    public List<String> id_products();
-    public Product saleProduct(String producto);
-    public ArrayList<ArrayList<String>> mostSales();
-    public Product stockAvaliable(Product product);
-    public boolean registred(Product product);
+    List<String> names_products();
+    List<String> id_products();
+    Product saleProduct(String producto);
+    ArrayList<ArrayList<String>> mostSales();
+    Product stockAvaliable(Product product);
+    boolean registred(Product product);
 }
