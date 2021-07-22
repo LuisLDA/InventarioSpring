8bda10842ef2ec0887d958eb06fdd3701451e8c4
LuisLDA <65144234+LuisLDA@users.noreply.github.com>
2021-07-22 17:51:48 -0500

    Agregado un hola mundo como comentaro

--- src/main/java/com/inventario/sistemainv/SistemainvApplication.java
+++ src/main/java/com/inventario/sistemainv/SistemainvApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SistemainvApplication {
 
     public static void main(String[] args) {
+        //HELLO WORLD
         SpringApplication.run(SistemainvApplication.class, args);
     }
 
